4051bd5d22fe0f406639fa0bd22f1b22493993a9
SantiCalabro <santi.calabro93@gmail.com>
2023-05-23 12:51:54 -0300

    cambiando el host en db.js (antes localhost)

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-// const server = require("./src/app.js");
-// const server = require("./src/app.js");
+// const server = require("./src/App.js");
 const { conn } = require("./src/db.js");
-import { listen } from "./src/app.js";
+
+import { server } from "./src/app.js";
+// import { conn } from "./src/db.js";
+
+// const server = require("./src/app.js");
+// const { conn } = require("./src/db.js");
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
   server.listen(3001, () => {
-    console.log("%s listening at 3001 "); // eslint-disable-line no-console
+    console.log("%s listening at 3001"); // eslint-disable-line no-console
   });
 });
