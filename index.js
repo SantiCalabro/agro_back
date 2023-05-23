abfb913360f2286e74fc274e8bc59da0c7b261f7
SantiCalabro <santi.calabro93@gmail.com>
2023-05-23 12:36:33 -0300

    import

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const server = require("./src/app.js");
-const { conn } = require("./src/db.js");
+// const server = require("./src/app.js");
+import { listen } from "./src/app.js";
+import { conn } from "./src/db.js";
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(3001, () => {
+  listen(3001, () => {
     console.log("%s listening at 3001 "); // eslint-disable-line no-console
   });
 });
