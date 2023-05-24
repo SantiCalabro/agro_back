7c08574e8ce1d95a1252afc4c98dca32d25a74d0
SantiCalabro <santi.calabro93@gmail.com>
2023-05-24 19:16:57 -0300

    prueba

--- src/app.js
+++ src/app.js
@@ -15,10 +15,7 @@ var corsOptions = {
   optionsSuccessStatus: 204,
 };
 server.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "agro.c13rc7bimzpk.us-east-1.rds.amazonaws.com"
-  );
+  res.header("Access-Control-Allow-Origin", "http://54.82.181.79:3036/");
   res.header("Access-Control-Allow-Credentials", true);
   res.header(
     "Access-Control-Allow-Headers",
